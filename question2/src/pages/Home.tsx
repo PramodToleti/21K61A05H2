833e15ab88bf4299e415af86fa91572b0ff51396
PramodToleti <pramodraina047@gmail.com>
2024-08-16 13:43:12 +0530

    added routes and ss

--- question2/src/pages/Home.tsx
+++ question2/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 type Product = {
   id: string;
-  name: string;
+  productName: string;
   price: string;
   rating: string;
   availability: string;
@@ -25,19 +26,28 @@ const Home = () => {
 
   return (
     <div className="p-10">
-      <div className="grid grid-cols-3 md:grid-cols-4 gap-5">
+      <div className="flex justify-end px-10">
+        <Link to="/" className="p-2 bg-gray-200 rounded-md mb-4">
+          Home
+        </Link>
+      </div>
+      <div className="grid xsm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {products.map((product: Product) => (
-          <div key={product.id} className="p-5 bg-gray-100 rounded-md">
+          <Link
+            to={`/product/${product.id}`}
+            key={product.id}
+            className="p-5 bg-gray-100 rounded-md"
+          >
             <img
               src="https://api.api-ninjas.com/v1/randomimage?category=nature"
-              alt={product.name}
+              alt={product.productName}
               className="w-full h-20 object-cover rounded-md"
             />
-            <h1 className="text-lg font-bold">{product.name}</h1>
-            <p className="text-sm">{product.price}</p>
-            <p className="text-sm">{product.rating}</p>
-            <p className="text-sm">{product.availability}</p>
-          </div>
+            <h1 className="text-lg font-bold">{product.productName}</h1>
+            <p className="text-sm">Name: {product.price}</p>
+            <p className="text-sm">Rating: {product.rating}</p>
+            <p className="text-sm">Availability: {product.availability}</p>
+          </Link>
         ))}
       </div>
     </div>
