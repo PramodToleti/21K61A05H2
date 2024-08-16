f61d48da8ffd40bd776df53232347045ec7b7f3d
PramodToleti <pramodraina047@gmail.com>
2024-08-16 13:23:08 +0530

    fix cors issue

--- question1/server.js
+++ question1/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const router = require("./routes/routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/health", (_, res) => {
   res.send("200 OK");
